f5c2991c90495aeb745a16929ebfb28f143d9615
Ryanyan89 <78816839+Ryanyan89@users.noreply.github.com>
2021-02-15 16:40:37 -0800

    Finished Game

--- Rock Paper Scissors Game/src/Main.java
+++ Rock Paper Scissors Game/src/Main.java
@@ -1,22 +1,71 @@
-import java.awt.print.Paper;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Rock Paper Scissors");
-        String Rock = "Rock";
-        String Scissors = "Scissors";
-        String Paper = "Paper";
-        int randomNumber, playerNumber;
 
-        randomNumber = (int)(Math.random() * 3 + 1);
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Choose 1 for Rock, 2 for Paper or 3 for Scissors.");
 
-        if (randomNumber == 1)
-            System.out.println("Bot played: " + Rock);
-        if (randomNumber == 2)
-            System.out.println("Bot played: " + Scissors);
-        if (randomNumber == 3)
-            System.out.println("Bot played: " + Paper);
+        int computerNumber, playerNumber, rock, paper, scissors;
+        rock = 1;
+        paper = 2;
+        scissors = 3;
 
+        //where player and computer chose number
+        playerNumber = scan.nextInt();
+        //if player does not chose number between 1 and 3
+        while (playerNumber != 1 && playerNumber != 2 && playerNumber != 3){
 
+            System.out.println("Choose 1 for Rock, 2 for Paper or 3 for Scissors.");
+            playerNumber = scan.nextInt();
+        }
+        computerNumber = (int) (Math.random() * 3 + 1);
+
+        //possible outcomes
+        if (playerNumber == rock && computerNumber == rock) {
+            System.out.println("You chose Rock");
+            System.out.println("Computer chose Rock");
+            System.out.println("Tie");
+        }
+        else if (playerNumber == paper && computerNumber == paper) {
+            System.out.println("You chose Paper");
+            System.out.println("Computer chose Paper");
+            System.out.println("Tie");
+        }
+        else if (playerNumber == scissors && computerNumber == scissors) {
+            System.out.println("You chose Scissors");
+            System.out.println("Computer chose Scissors");
+            System.out.println("Tie");
+        }
+        else if (playerNumber == rock && computerNumber == paper) {
+            System.out.println("You chose Rock");
+            System.out.println("Computer chose Paper");
+            System.out.println("You lost");
+        }
+        else if (playerNumber == paper && computerNumber == rock) {
+            System.out.println("You chose Paper");
+            System.out.println("Computer chose Rock");
+            System.out.println("You Win");
+        }
+        else if (playerNumber == scissors && computerNumber == rock) {
+            System.out.println("You chose Scissors");
+            System.out.println("Computer chose Rock");
+            System.out.println("You lost");
+        }
+        else if (playerNumber == rock && computerNumber == scissors) {
+            System.out.println("You chose Rock");
+            System.out.println("Computer chose Scissors");
+            System.out.println("You win");
+        }
+        else if (playerNumber == scissors && computerNumber == paper) {
+            System.out.println("Ypu chose Scissors");
+            System.out.println("Computer chose Paper");
+            System.out.println("You win");
+        }
+        else if (playerNumber == paper && computerNumber == scissors) {
+            System.out.println("You chose Paper");
+            System.out.println("Computer chose Scissors");
+            System.out.println("You lost");
+        }
     }
 }
