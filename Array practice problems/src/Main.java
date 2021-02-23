16ff28f5eacc980f438934af5672b59d49004773
Ryanyan89 <78816839+Ryanyan89@users.noreply.github.com>
2021-02-23 01:13:25 -0800

    Array practice Finished

--- Array practice problems/src/Main.java
+++ Array practice problems/src/Main.java
@@ -1,22 +1,46 @@
 public class Main {
     public static void main(String[] args) {
+        //problem 1
         int[] smallNumbers = new int[20];
+        int numberOne = 0;
         for(int i = 0 ; i < smallNumbers.length; i++){
-            int random = (int) (Math.random()*6 + 1);
-            smallNumbers[i] = random;
-            System.out.println("Index: " + i + " Value: " + random);
+            smallNumbers[i] = (int) (Math.random()*6 + 1);
+            System.out.println("Index: " + i + "\tValue: " + smallNumbers[i]);
+            if(smallNumbers[i] == 1){
+                numberOne++;
+            }
         }
+        System.out.println("amount of ONES: " + numberOne);
+        //problem 2
         int [] bigNumbers = new int[10];
         for(int i = 0; i < bigNumbers.length; i++){
-            int rngNumber = (int) (Math.random()*100 + 1);
-            bigNumbers[i] = rngNumber;
-            System.out.println("Index: " + i + " Value: " + rngNumber);
+            bigNumbers[i] = (int) (Math.random()*100 + 1);
+            System.out.println("Index: " + i + "\tValue: " + bigNumbers[i]);
         }
         int total = 0;
-        for(int bigNumber : bigNumbers) {
-            total = total + bigNumber;
+        for(int i = 0; i < bigNumbers.length; i++) {
+            total = total + bigNumbers[i];
         }
         int average  = total / bigNumbers.length;
-        System.out.println(average);
+        System.out.println("average number: " + average);
+
+        //problem 3
+        int firstNumber = bigNumbers[0];
+        int lastNumber = bigNumbers[9];
+        bigNumbers[0] = lastNumber;
+        bigNumbers[9] = firstNumber;
+        System.out.println("index: 0 \tValue: " + bigNumbers[0]);
+        System.out.println("index: 9 \tValue: " + bigNumbers[9] + "\n");
+
+        //problem 4
+        for(int i = 0; i < bigNumbers.length; i++) {
+            System.out.println("Index: " + i + "\tValue: " + bigNumbers[i]);
+        }
+        System.out.println();
+        int [] backwards = new int[10];
+        for(int i = 0; i < backwards.length; i++){
+            backwards[i] = bigNumbers[9 - i];
+            System.out.println("Index: " + i + "\tReversed Value: " + backwards[i]);
+        }
     }
 }
