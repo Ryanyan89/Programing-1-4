c7f87de5faee7554ad4a2dc0841be73e609fd32c
Ryanyan89 <78816839+Ryanyan89@users.noreply.github.com>
2021-02-24 23:42:53 -0800

    Finished Array and method assignment

--- Array and method assignemnt/src/Main.java
+++ Array and method assignemnt/src/Main.java
@@ -1,20 +1,73 @@
 public class Main {
     public static int[] addNumberToArray(int[] inputArray, int number) {
         int[] outputArray = new int[inputArray.length + 1];
-        for (int i = 0; i < inputArray.length; i++){
-            inputArray[i] = inputArray[i] + 1;
+        for(int i = 0; i < inputArray.length; i++){
+            outputArray[i] = inputArray[i];
+            outputArray[outputArray.length - 1] = number;
         }
-        // in a for loop copy the inputArray to the output array
-        // return the array
         return outputArray;
     }
+
     public static int[] deleteNumber(int[] inputArray){
-
+        int[] outputArray = new int[inputArray.length - 1];
+        for(int i = 0; i < outputArray.length; i++){
+            outputArray[i] = inputArray[i];
+        }
+        return  outputArray;
     }
-    public static int[] insertNumber(int[] inputArray, int number){
+    public static int[] insertNumber(int[] inputArray, int number, int indexPosition){
+        int[] outputArray = new int[inputArray.length + 1];
+        for (int i = 0; i < outputArray.length; i++) {
+            //check if i is equal to indexPosition - 1
+            if(i == indexPosition - 1){
+                outputArray[indexPosition - 1] = number;
+            }
+            //check if number is inserted
+            else if(outputArray[indexPosition - 1] == number){
+                outputArray[i] = inputArray[i - 1];
+            }
+            //adding values to the array until number is inserted
+            else{
+                outputArray[i] = inputArray[i];
+            }
 
+        }
+
+        return outputArray;
     }
     public static void main(String[] args) {
+        //adding of a index position to the end of an array
+        int[] newArray = new int[5];
+        for(int i = 0; i < newArray.length; i++) {
+            newArray[i] = i + 1;
+        }
+        int[] newLine = addNumberToArray(newArray, 727);
+        for(int value : newLine){
+            System.out.println(value);
+        }
+        System.out.println();
+        //getting rid of an index position at the end of an array
+        int[] newArray2 = new int[5];
+        for(int i = 0; i < newArray2.length; i++) {
+            newArray2[i] = i + 1;
+        }
+        int[] goneLine = deleteNumber(newArray2);
+        for(int value : goneLine){
+            System.out.println(value);
+        }
+        System.out.println("");
+        //inserting a index position in an point of the array
+        int[] newArray3 = new int[5];
+        for(int i = 0; i < newArray2.length; i++){
+            newArray3[i] = i + 1;
+        }
+        int[] insertNumber =insertNumber(newArray3, 420, 3);
+        for(int value : insertNumber){
+            System.out.println(value);
+        }
+
+
+
 
     }
 }
