9ce759241caab18826dc83182abca0db603084dd
Ryan Yan <798528@learn.vsb.bc.ca>
2021-03-10 10:27:37 -0800

    Time assignment in class work

---
+++ Time assignment/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Time time = new Time(2, 20, 40);
+        time.setSecond(12);
+    }
+}
---
+++ Time assignment/src/Time.java
@@ -0,0 +1,74 @@
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        if (hour > 24){
+            int hour1;
+            hour1 = hour % 24;
+            this.hour = hour1;
+        }
+        else{
+            this.hour = hour;
+        }
+
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        if (minute > 60){
+            int minute1, increaseHour;
+            minute1 = minute % 60;
+            increaseHour = minute/60;
+            this.minute += increaseHour;
+            this.minute = minute1;
+        }
+        else {
+            this.minute = minute;
+        }
+
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        if (second > 60){
+            int second1, increaseMinute;
+            second1 = second % 60;
+            increaseMinute = second/60;
+            this.minute += increaseMinute;
+            this.second = second1;
+        }
+        else{
+            this.second = second;
+        }
+
+    }
+
+    public void setTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public String toString(){
+        
+    }
+
+
+}
