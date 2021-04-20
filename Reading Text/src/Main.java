40235e824891a4bb9bb3daac6bd1409adf6d7ded
Ryan Yan <798528@learn.vsb.bc.ca>
2021-04-20 10:25:45 -0700

    Things

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -1,46 +1,42 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class Main {
-    public String check(String word) throws IOException {
-        FileReader fr = new FileReader("ProgrammingHistory.txt");
-        BufferedReader br = new BufferedReader(fr);
-        ArrayList<String> lines = new ArrayList<>();
-        ArrayList<String> sentences = new ArrayList<>();
-        while ((line = br.readLine()) !=null){
-            lines.add(line);
+    public static Object check(String word, ArrayList<String> arrayList, ArrayList<Integer> certainLine){
+        for (int i = 0; i < arrayList.size(); i++){
+            if (arrayList.get(i).contains(word)){
+                certainLine.add(i);
+            }
         }
-        for (int i = 0; i < )
-        return
+        for (int i = 0; i < certainLine.size(); i++){
+            return certainLine.get(i);
+        }
+        return null;
     }
     public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("ProgrammingHistory.txt");
         BufferedReader br = new BufferedReader(fr);
         ArrayList<String> lines = new ArrayList<>();
         ArrayList<String> sentences = new ArrayList<>();
+        ArrayList<Integer> certainLine = new ArrayList<>();
+        String sentence = " ";
         String line;
-        int firstIndex = 0;
-        int lastIndex = 1;
         while ((line = br.readLine()) !=null){
             lines.add(line);
         }
         br.close();
         for (int i = 0; i < lines.size(); i++) {
             System.out.println(lines.get(i));
-            lastIndex++;
-            if ( lines.get(i).equals(".") && lines.get(i + 1).equals(" ")){
-                String sentence = line.substring(firstIndex, lastIndex);
-                sentences.add(sentence);
-                firstIndex = Integer.parseInt(lines.get(i + 2));
-            }
-            else if (i == lines.size()-1){
-                String sentence = line.substring(firstIndex, lastIndex);
-                sentences.add(sentence);
-            }
+
         }
+        System.out.println("*********************************************");
+        System.out.println("\n");
+        System.out.println(check("the", lines, certainLine))
+        System.out.println("\n");
+        System.out.println("*********************************************");
+
     }
 }
