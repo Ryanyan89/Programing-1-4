2c68b629fffce81300c7d40c34d366fea7ff83a6
Ryan Yan <798528@learn.vsb.bc.ca>
2021-04-21 10:24:21 -0700

    sad

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 
 public class Main {
-    public static void check(String word, ArrayList<String> arrayList, ArrayList<Integer> answers) {
+    public static String check(String word, ArrayList<String> arrayList, ArrayList<Integer> answers) {
         int lineNumber = 0;
         for (int i = 0; i < arrayList.size(); i++) {
             lineNumber++;
@@ -13,27 +13,79 @@ public class Main {
                 answers.add(lineNumber);
             }
         }
+        if (answers.size() == 0) {
+            return "Your word " + word + " was not found.";
+        }
+        else {
+            return String.valueOf(answers);
+        }
     }
     public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("ProgrammingHistory.txt");
         BufferedReader br = new BufferedReader(fr);
         ArrayList<String> lines = new ArrayList<>();
-
         ArrayList<Integer> answers = new ArrayList<>();
-        ArrayList<String> possibleSentence = new ArrayList<>();
+        ArrayList<String> sentences = new ArrayList<>();
+
+        int period;
+        int bracket;
+        int firstIndex = 0;
+        int periods = 0;
+        String fullSentence = " ";
+        String sentence;
         String line;
-        String word = "the";
+        String word = "once";
         while ((line = br.readLine()) !=null){
             lines.add(line);
         }
         br.close();
         for (int i = 0; i < lines.size(); i++) {
             System.out.println(lines.get(i));
-        }
-        check("the", lines, answers);System.out.println("Your word: " + word + ". Find your word in lines: " );
-        for (int i = 0; i < answers.size(); i++) {
-            System.out.println(answers.get(i));
+            sentence = lines.get(i);
+            char[] letters = new char[sentence.length()];
+            letters = sentence.toCharArray();
+            period = sentence.indexOf(".");
+            bracket = sentence.indexOf("]");
+            for (int i2 = 0; i2 < letters.length; i2++){
+                if (letters[i2] == '.'){
+                    periods++;
+                    if (periods > 1){
+                        assert false;
+                        period = fullSentence.indexOf(".");
+                        bracket = fullSentence.indexOf("]");
+                        if (period + 1 == fullSentence.length()){
+                            periods = 1;
+                        }
+                        else{
+                            if (letters[i2] == '.' && letters[i2 + 1] == '['){
+                                fullSentence = sentence.substring(firstIndex, bracket + 1);
+                                sentences.add(fullSentence);
+                                firstIndex = bracket;
+                            }
+                            else if (letters[i2] == '.' && letters[i2 + 1] != '['){
+                                fullSentence = sentence.substring(firstIndex, period + 1);
+                                sentences.add(fullSentence);
+                                firstIndex = period;
+                            }
+                        }
+                    }
+                }
+            }
+            if (periods == 1){
+                sentences.add(sentence);
+            }
+
+            periods = 0;
+            firstIndex= 0;
+
         }
 
+        System.out.println("***************************************************");
+        System.out.println("Your is word: " + word + ". Find your word in lines: " );
+        System.out.println(check(word, lines, answers));
+        System.out.println("***************************************************");
+        for (int i = 0; i <sentences.size(); i++) {
+            System.out.println(sentences.get(i));
+        }
     }
 }
