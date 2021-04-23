b62c17a277a8e4fbf409d0c4b2da6dcb4b3cebf4
Ryanyan89 <78816839+Ryanyan89@users.noreply.github.com>
2021-04-22 18:54:33 -0700

    Finished

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -1,81 +1,77 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class Main {
-    public static String check(String word, ArrayList<String> arrayList, ArrayList<Integer> answers) {
-        int lineNumber = 0;
-        for (int i = 0; i < arrayList.size(); i++) {
-            lineNumber++;
-            if (arrayList.get(i).contains(word)){
-                answers.add(lineNumber);
+    public static ArrayList<Integer> findWord(ArrayList<String> sentences, String word) {
+        ArrayList<Integer> indexPos = new ArrayList<>();
+        for (int i = 0; i < sentences.size(); i++) {
+            for (int j = 0 ; j < sentences.get(i).length(); j++) {
+                if (sentences.get(i).startsWith(word, j)) {
+                    indexPos.add(i);
+                    indexPos.add(j);
+                }
             }
         }
-        if (answers.size() == 0) {
-            return "Your word " + word + " was not found.";
-        }
-        else {
-            return String.valueOf(answers);
-        }
+        return indexPos;
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<String> sentences = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
         FileReader fr = new FileReader("ProgrammingHistory.txt");
         BufferedReader br = new BufferedReader(fr);
-        ArrayList<String> lines = new ArrayList<>();
-        ArrayList<Integer> answers = new ArrayList<>();
-        ArrayList<String> sentences = new ArrayList<>();
-        int period;
-        int bracket;
-        int firstIndex = 0;
-        String fullSentence;
-        String sentence;
+
         String line;
-        String word = "once";
-        while ((line = br.readLine()) !=null){
-            lines.add(line);
+        while ((line = br.readLine()) != null) {
+            if (!line.equals("")) {
+                lines.add(line);
+            }
         }
         br.close();
-        for (int i = 0; i < lines.size(); i++) {
-            System.out.println(lines.get(i));
-            sentence = lines.get(i);
-            period = sentence.indexOf(".");
-            bracket = sentence.indexOf("]");
-            if (period + 1 == sentence.length()){
-                sentences.add(sentence);
-            }
-            else if (bracket + 1 == sentence.length()){
-                sentences.add(sentence);
-            }
-            else {
-                if (bracket > period){
-                    fullSentence = sentence.substring(firstIndex, bracket + 1);
-                    firstIndex = bracket + 2;
+
+        for (String s : lines) {
+            int counter = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '.' && !s.startsWith(".g", i) && !s.startsWith(".,", i)) {
+                    if (s.length()-1 != i) {
+                        if (s.charAt(i + 1) == '[') {
+                            sentences.add(s.substring(counter, i+4));
+                        }
+                        else {
+                            sentences.add(s.substring(counter, i+1));
+                        }
+                    }
+                    else {
+                        sentences.add(s.substring(counter, i+1));
+                    }
+                    if (s.length()-1 != i) {
+                        if (s.charAt(i+1) == ' ') {
+                            counter = i+2;
+                        }
+                        else if (s.charAt(i+1) == '[') {
+                            counter = i+5;
+                        }
+                    }
+                    else {
+                        counter = i+1;
+                    }
                 }
-                else {
-                    fullSentence = sentence.substring(firstIndex, period + 1);
-                    firstIndex = period + 2;
-                }
-                sentences.add(fullSentence);
-                while (period + 1 <= fullSentence.length()){
-                    fullSentence = sentence.substring(firstIndex);
-                    period = fullSentence.indexOf(".");
-                    firstIndex = 0;
-                    fullSentence = fullSentence.substring(firstIndex, period + 1);
-                    firstIndex = period + 2;
-                    sentences.add(fullSentence);
-                }
-                firstIndex = 0;
             }
         }
 
-        System.out.println("***************************************************");
-        System.out.println("Your is word: " + word + ". Find your word in lines: " );
-        System.out.println(check(word, lines, answers));
-        System.out.println("***************************************************");
-        for (int i = 0; i <sentences.size(); i++) {
-            System.out.println(sentences.get(i));
+        for (String s : sentences) {
+            System.out.println(s);
         }
+
+        String word = "the";
+        ArrayList<Integer> indexPos = findWord(sentences, word);
+        for (int i = 0; i < indexPos.size(); i += 2) {
+            System.out.println("The word \"" + word + "\" appears in sentence: #" + indexPos.get(i) + " in Index Position: " + indexPos.get(i+1));
+        }
+
+        System.out.println("\"" + word + "\" appears " + indexPos.size()/2 + " times");
+
     }
 }
